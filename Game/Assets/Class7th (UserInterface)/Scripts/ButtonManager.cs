cc0af5532d4d858b31aa35c38ec01f7ed0ce076f
jaeyoon <dudwns1927@gmail.com>
2025-04-21 18:00:41 +0900

    (2025.4.18) 유저 인터페이스

---
+++ Game/Assets/Class7th (UserInterface)/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void Excute() {
+
+       Debug.Log("Execute");
+    }
+
+    public void Shop() {
+
+       Debug.Log("Shop");
+    }
+
+    public void Quit() {
+        
+       Debug.Log("Quit");
+    }
+}
