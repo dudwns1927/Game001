9728b72b3f2702e18fbe540e52c254d4e2084fd1
jaeyoon <dudwns1927@gmail.com>
2025-04-22 20:09:03 +0900

    (2025.4.22) 이동 시스템

---
+++ Game/Assets/Class8th (Movement)/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    void Start() {
+        transform.position = new Vector3(1, 1, 1);
+    
+        Debug.Log(transform.position);
+    
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.W)) {
+            transform.position += new Vector3(0, 0, 1);
+            Debug.Log("W");
+        }
+    }
+
+
+}
