e33d1470f0292e8517ff7beb34a8dbedd0eb5733
jaeyoon <dudwns1927@gmail.com>
2025-04-23 19:45:39 +0900

    (2025.4.23) 충돌 시스템

--- Game/Assets/Class8th (Movement)/Scripts/Movement.cs
+++ Game/Assets/Class8th (Movement)/Scripts/Movement.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
+    /*
     void Start() {
         transform.position = new Vector3(1, 1, 1);
     
@@ -17,7 +18,35 @@ public class Movement : MonoBehaviour
             transform.position += new Vector3(0, 0, 1);
             Debug.Log("W");
         }
+
+        if(Input.GetKeyDown(KeyCode.A)) {
+            transform.position += new Vector3(-1, 0, 0);
+            Debug.Log("A");
+        }
+
+        if(Input.GetKeyDown(KeyCode.S)) {
+            transform.position += new Vector3(0, 0, -1);
+            Debug.Log("S");
+        }
+
+        if(Input.GetKeyDown(KeyCode.D)) {
+            transform.position += new Vector3(1, 0, 0);
+            Debug.Log("D");
+        }
     }
 
+    */
+    [SerializeField] Vector3 direction;
+    [SerializeField] float speed = 5.0f;
+    
+    
+    void Update() {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
 
+        direction.Normalize();
+
+        transform.position = transform.position + direction * speed * Time.deltaTime;
+
+    }
 }
