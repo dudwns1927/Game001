489bbdd77f56bb8c09c82a097ce2d212e6ac0889
jaeyoon <dudwns1927@gmail.com>
2025-04-30 19:44:58 +0900

    (2025.4.30) 조명 시스템

--- Game/Assets/Class10th (Instantiate & Destroy/Scripts/Weapon Manager.cs
+++ Game/Assets/Class10th (Instantiate & Destroy/Scripts/Weapon Manager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
@@ -9,12 +11,42 @@ public class WeaponManager : MonoBehaviour
     
     [SerializeField] Transform parentPosition;
 
-    void start() {
-        for (int i = 0; i < weapons.Length; i++) {
-            GameObject weapon = Instantiate(weapons[i], parentPosition);
-            weaponList.Add(weapon);
+    [SerializeField] int count = 0;
+    int currentIndex = 0;
+
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Swap();
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Attack();
         }
     }
 
+    public void Swap() {
+        weaponList[count].gameObject.SetActive(false);
+
+        count = (count + 1) % weaponList.Count;
+
+        weaponList[count].gameObject.SetActive(true);
+    }
+
+
+    void Start() {
+        for (int i = 0; i < weapons.Length; i++) {
+            GameObject clone = Instantiate(weapons[i], parentPosition);
+            clone.gameObject.SetActive(false);
+            weaponList.Add(clone);
+        }
+
+        weaponList[count].gameObject.SetActive(true);
+    }
+
+    public void Attack() {
+        weaponList[count].GetComponent<Weapon>().Attack();
+       // Attack logic here
+        Debug.Log("Attacking with " + weaponList[count].name);
+    }
 
 }
