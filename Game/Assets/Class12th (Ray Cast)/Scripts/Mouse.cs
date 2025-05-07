644fee7c351fbb05f04edbe6239c509fc7dcff42
jaeyoon <dudwns1927@gmail.com>
2025-05-07 19:45:38 +0900

    (2025.5.7) 파티클 시스템

--- Game/Assets/Class12th (Ray Cast)/Scripts/Mouse.cs
+++ Game/Assets/Class12th (Ray Cast)/Scripts/Mouse.cs
@@ -6,6 +6,7 @@ public class Mouse : MonoBehaviour
 {
     [SerializeField] Ray ray;
     [SerializeField] RaycastHit raycastHit;
+    [SerializeField] LayerMask layerMask;
 
     private void Update() {
         if (Input.GetMouseButtonDown(0))
@@ -14,13 +15,23 @@ public class Mouse : MonoBehaviour
 
             //Debug.Log(ray.origin);
 
-            if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+            if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity, layerMask))
             {
-                raycastHit.collider.GetComponent<Renderer>().material.color = Color.red;
+                GameObject prefab = raycastHit.collider.gameObject;
+
+                if(prefab.transform.GetChild(0).gameObject.activeSelf)
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(false);
+                }
+                else
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(true);
+                }
+
+                //raycastHit.collider.GetComponent<Renderer>().material.color = Color.red;
                 Debug.Log(raycastHit.collider.gameObject.name);
                 Debug.Log(raycastHit.point);
-                Debug.Log(raycastHit.normal);
-                Debug.Log(raycastHit.distance);
+
             }
         }
     }
